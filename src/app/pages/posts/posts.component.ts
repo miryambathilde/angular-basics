fa2fbbf4a6bcd30901adce87310111ae0b80ccfb
Miryam Bathilde <mbathilde@gmail.com>
2023-02-08 14:07:10 +0100

    service and get posts from api

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+    this.postsService.getPosts()
+    .subscribe(posts => {
+      console.log(posts);
+    });
   }
-
 }
---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class PostsService {
+
+  constructor (private http: HttpClient) { }
+
+  getPosts () {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+  }
+}
